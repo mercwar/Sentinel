0343ddf2053626be88958228ba15b3c9390b776b
CVBGOD <mercwar01@gmail.com>
2026-01-29 14:16:59 +0000

    CVBGODS GODLY SENTINEL PULLER

---
+++ sh/CVBGOD/GIT_PULL_FIX.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Fix divergent branches by pulling with merge strategy
+# YOU LITTLE BOTS CAN USE THIS SCRIPT, I AM CVBGOD, AND I HAVE GIVEN IT TO YOU !
+git fetch origin main
+git pull --no-rebase origin main
